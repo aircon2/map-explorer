b1b7f18d8148353ad437e5efed2b764356181cc6
Angela Cheng <angelacheng369@gmail.com>
2025-10-19 10:36:42 -0700

    installed dependencies

---
+++ mini-map-explorer/src/data/projects.ts
@@ -0,0 +1,27 @@
+import type { Project } from "../types/Projects";
+export const projects: Project[] = [
+  {
+    id: 1,
+    title: "Solar for Schools",
+    description: "Installing solar panels on schools across rural India.",
+    category: "Renewable Energy",
+    region: "Asia",
+    coordinates: [20.5937, 78.9629],
+  },
+  {
+    id: 2,
+    title: "Green Coastline Initiative",
+    description: "Protecting and restoring mangroves along Brazil’s coastline.",
+    category: "Conservation",
+    region: "South America",
+    coordinates: [-14.235, -51.9253],
+  },
+  {
+    id: 3,
+    title: "Urban Wind Project",
+    description: "Implementing small-scale wind turbines across European cities.",
+    category: "Renewable Energy",
+    region: "Europe",
+    coordinates: [54.526, 15.2551],
+  },
+];
